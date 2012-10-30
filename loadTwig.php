bcaf013b4cb53d29252f277de4e43dbbb3cbe79a
Aurélien Bertron <aure_bertron@hotmail.com>
2012-10-30 17:18:34 +0100

    Ajoute du contenu

--- loadTwig.php
+++ loadTwig.php
@@ -23,10 +23,25 @@
 		}
 	}
 
-	// Renvoit la racine du site (pour la construction d'URL)
-	function root()
-	{
+// Fonctions de construction d'URL
+	function root() {
 		return "http://".$_SERVER['SERVER_NAME'];
+	}
+	function templates() {
+		return root()."/templates";
+	}
+	function css() {
+		return templates()."/style";
+	}
+	function images() {
+		return css()."/images";
+	}
+	function queries($module, $action, $param) {
+		$query = root()."/index.php?module=".$module."&action=".$action;
+		foreach($param as $key => $value) {
+			$query .= "&".$key."=".$value;
+		}
+		return $query;
 	}
 	
 	Twig_Autoloader::register();
@@ -38,4 +53,9 @@
 	// Fonction de redirection d'URL : renvoit l'URL sauvegardée précédemment (avec &redirect=true)
 	$twig->addFunction("redirectURL", new Twig_Function_Function("redirectURL"));
 
+	// Fonctions de construction d'URL
 	$twig->addFunction("root", new Twig_Function_Function("root"));
+	$twig->addFunction("templates", new Twig_Function_Function("templates"));
+	$twig->addFunction("css", new Twig_Function_Function("css"));
+	$twig->addFunction("images", new Twig_Function_Function("images"));
+	$twig->addFunction("queries", new Twig_Function_Function("queries"));
